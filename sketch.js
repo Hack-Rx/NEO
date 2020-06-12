f9c428d30290f2790adc8e542c489316eae92862
ParthB <41703901+ParthBasole@users.noreply.github.com>
2020-06-12 18:23:19 +0530

    object_hitting_Ar_0.1.0

--- sketch.js
+++ sketch.js
@@ -1,60 +1,127 @@
+// Copyright (c) 2018 ml5
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+// altered code by Luca Damasco 2 luxapodular
+
+/* ===
+ml5 Example
+PoseNet example using p5.js
+=== */
+
 let video;
-let posNet;
-let noseX;
-let noseY;
-let Leye_X=0;
-let Leye_Y=0;
-let Reye_X=0;
-let Reye_Y=0;
-let Dist;
-function setup(){
-createCanvas(648,480);
-  video= createCapture(VIDEO);
+let poseNet;
+let poses = [];
+
+// Storing the last keypoint position
+let lastKeypoints = [];
+
+function setup() {
+  
+  
+  infront = loadSound('infront.mp3');
+  behind = loadSound('behind.mp3');
+  noone = loadSound('noone.mp3');
+  
+  createCanvas(640, 480);
+  video = createCapture(VIDEO);
+  video.size(width, height);
+
+  // Create a new poseNet method with a single detection
+  poseNet = ml5.poseNet(video);
+  // This sets up an event that fills the global variable "poses"
+  // with an array every time new poses are detected
+  poseNet.on('pose', function(results) {
+    poses = results;
+  });
+  // Hide the video element, and just show the canvas
   video.hide();
-  posNet=ml5.poseNet(video);
-  posNet.on('pose',gotPoses);
+	
+  print(lastKeypoints); 
+	// setup original keypoints
+	createDefaultKeypoints(); 
+  print(lastKeypoints);
 }
 
-function gotPoses(poses){
-  console.log(poses);
-  console.log('making sense');
-if(poses.length>0)
-{
-  let nX=poses[0].pose.keypoints[0].position.x;
-  let nY=poses[0].pose.keypoints[0].position.y;
-  let eXL=poses[0].pose.keypoints[1].position.x;
-  let eYL=poses[0].pose.keypoints[1].position.y;
-  let eXR=poses[0].pose.keypoints[2].position.x;
-  let eYR=poses[0].pose.keypoints[2].position.y;
-  Leye_X=eXL;
-  Leye_Y=eYL;
-  Reye_X=eXR;
-  Reye_Y=eYR;
-  noseX=nX;
-  noseY=nY;
-  
-  console.log(noseX);
-  console.log(noseY);
-  /*ellipse(Leye_X,Leye_Y,50);
-  ellipse(Reye_X,Reye_Y,50);
-  */
-}
+// Create default keypoints for easing. 
+function createDefaultKeypoints() {
+	let numKeypoints = 17; 
+	for (let i=0; i<numKeypoints; i++) {
+		newKeypoint = {x:width/2,
+									 y:height/2}
+    
+		lastKeypoints.push(newKeypoint);
+	}
 }
 
-function draw(){
-  background(220);
-  image(video,0,0);
-  fill(255,0,0);
-  Dist=dist(Leye_X,Leye_Y,Reye_X,Reye_Y);
-  Dist=0.75*Dist;
-  ellipse(noseX,noseY,Dist);
-  //ellipse(Leye_X,Leye_Y,Dist);
-  //ellipse(Reye_X,Reye_Y,Dist);
-  
+function updateKeypoints() {
+	// If there are no poses, ignore it.
+	if (poses.length <= 0) {
+		return; 
+	}
+	
+	// Otherwise, let's update the points; 
+	
+	let pose = poses[0].pose;
+	let keypoints = pose.keypoints; 
+	
+	for (let kp=0; kp<keypoints.length; kp++) {
+    
+		let oldKeypoint = lastKeypoints[kp];
+		let newKeypoint = keypoints[kp].position; 
+		
+		let interpX = lerp(oldKeypoint.x, newKeypoint.x, .3); 
+		let interpY = lerp(oldKeypoint.y, newKeypoint.y, .3); 
+		
+		let interpolatedKeypoint = {x: interpX,
+																y: interpY}
+		
+		lastKeypoints[kp] = interpolatedKeypoint; 
+	}
 }
-function modelReady()
-{
-  console.log('ready!');
+
+function draw() {
+  image(video, 0, 0, width, height);
+
+	updateKeypoints(); 
   
-  
+  drawKeypoints();
 }
+
+// A function to draw ellipses over the detected keypoints
+function drawKeypoints()  {
+  
+	for (let i=0; i<lastKeypoints.length; i++) {
+		keypoint = lastKeypoints[i]; 
+    fill(255,0,0); 
+    ellipse(keypoint.x, keypoint.y, 10,10); 
+	}
+}
+
+
+function someoneBehind() {
+  if (behind.isPlaying() or infront.isPlaying() or noone.isPlaying() ) {
+    //do nothing
+  } else {
+    behind.play();
+  }
+}
+
+
+function someoneInfront() {
+  if (behind.isPlaying() or infront.isPlaying() or noone.isPlaying() ) {
+    //do nothing
+  } else {
+    infront.play();
+  }
+}
+
+
+function noOne() {
+  if (behind.isPlaying() or infront.isPlaying() or noone.isPlaying() ) {
+    //do nothing
+  } else {
+    noone.play();
+  }
+}
+
